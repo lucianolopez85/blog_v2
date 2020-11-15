a00c655e89e203a040a7d46f3d7f7aef460d9dd2
Luciano <lopezz.lucio85@gmail.com>
2020-11-15 20:48:25 -0300

    editado templates

--- blog/settings.py
+++ blog/settings.py
@@ -89,10 +89,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 from django.contrib.messages import constants
@@ -107,3 +107,5 @@ MESSAGE_TAGS = {
 }
 
 INSTALLED_APPS += ('django_summernote',)
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,10 @@ from .models import Post
 
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 6
+    context_object_name = 'posts'
 
 class PostBusca(PostIndex):
     pass
