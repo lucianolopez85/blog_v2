1678f57d2bacfd102f93ae6a327333a84cfa0f97
Luciano <lopezz.lucio85@gmail.com>
2020-11-17 19:03:30 -0300

    editado models, l11

--- posts/views.py
+++ posts/views.py
@@ -2,19 +2,66 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
+from comentarios.forms import FormComentario
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 6
+    paginate_by = 3
     context_object_name = 'posts'
 
-class PostBusca(PostIndex):
-    pass
+    def get_queryset(self): 
+        qs = super().get_queryset()
+        qs = qs.order_by('-id')
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+        return qs
 
-class PostCategoria(PostIndex):
-    pass
+class PostBusca(PostIndex):
+    template_name = 'posts/post_busca.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo) |
+            Q(categoria_post__name_cat__iexact=termo)
+        )
+
+class PostCategoria(PostIndex): #sobre escrevendo a classe mãe PostIndex
+    template_name = 'posts/post_categoria.html'
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return qs
+
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+
+        return qs
+
 
 class PostDetalhes(UpdateView):
-    pass
+    template_name = 'posts/post_detalhes.html'
+    model = Post
+    form_class = FormComentario
+    context_object_name = 'post'
+    
